55c70cd9c3bfd129cf9fca812c449e3593a008c4
Cristian Urbina <metxito@gmail.com>
2025-09-03 22:00:56 +0000

    Update 02 example

--- code/02_automatic_simple_mirroring/01_create_files.py
+++ code/02_automatic_simple_mirroring/01_create_files.py
@@ -8,15 +8,18 @@ from azure.storage.filedatalake import DataLakeServiceClient
 
 
 tables = ["CardType"]
-config = json.load(open("../00_config/config.json"))
+config = json.load(open("00_config.json"))
 
 sql_source_engine = create_engine(f"mssql+pyodbc://{config["sql_user"]}:{config["sql_password"]}@{config["sql_server"]},{config["sql_port"]}/{config["sql_catalog_source"]}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes")
 with sql_source_engine.connect() as conn:
     for t in tables:
 
+
+
         # PREPARE folder
-        folder = f"../../results/01_simple_mirroring/{t}"
+        folder = f"../../results/02_automatic_simple_mirroring/{t}"
         os.makedirs(folder, exist_ok=True)
+
 
 
         # QUERY Unique keys
@@ -32,6 +35,37 @@ with sql_source_engine.connect() as conn:
             json.dump(json_data, f)
 
 
+
+        
+        
+        # GET the columns from the table
+        columns_query = f"SELECT [COLUMN_NAME] FROM [INFORMATION_SCHEMA].[COLUMNS] WHERE [TABLE_NAME] = '{t}'"
+        columns_df = pd.read_sql(columns_query, conn)
+        columns = columns_df["COLUMN_NAME"].tolist()
+
+        # GET max value of CreatedOn and/or ModifiedOn
+        new_timestamp = None
+        timestamp_column_used = None
+        there_is_modified_on = False
+        max_datetime = "MAX([CreatedOn])"
+        if "ModifiedOn" in columns:
+            max_datetime = f"CASE WHEN MAX([ModifiedOn]) IS NULL THEN MAX([CreatedOn]) WHEN MAX([ModifiedOn]) > MAX([CreatedOn]) THEN MAX([ModifiedOn]) ELSE MAX([CreatedOn]) END"
+            there_is_modified_on = True
+
+        max_query = f"SELECT FORMAT({max_datetime}, 'yyyy-MM-ddTHH:mm:ss.fffffff') AS [new_timestamp] FROM [dbo].[{t}]"
+        max_df = pd.read_sql(max_query, conn)
+        new_timestamp = max_df["new_timestamp"].iloc[0]
+
+        # SAVE to _current_timestamp.json 
+        timestamp_json_file = "_current_timestamp.json"
+        timestamp_json_path = os.path.join(folder, timestamp_json_file)
+        with open(timestamp_json_path, "w") as f:
+            json.dump({"current_timestamp": new_timestamp}, f)
+
+
+
+
+
         # QUERY data
         #,[__rowMarker__]=0  is not mandatory in the first insert. Actually is not recommended
         data_query = f"SELECT * FROM [dbo].[{t}]"
@@ -45,4 +79,4 @@ with sql_source_engine.connect() as conn:
 
 
 
-        print (f"Files for {t}")
+        print (f"Done")
---
+++ code/02_automatic_simple_mirroring/02_simulate_changes.sql
@@ -0,0 +1,9 @@
+SELECT * FROM [dbo].[CardType]
+
+
+UPDATE [dbo].[CardType] SET [Description]='Generic Debit Card' WHERE [CardTypeID]=5
+
+
+SELECT * FROM [dbo].[CardType]
+
+
